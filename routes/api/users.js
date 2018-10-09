5fb8be8542ea0620988a0a2e84ff1057f04469e6
Fuzoka <unbeatablesites09@gmail.com>
2018-10-08 22:48:33 -0400

    Adding project

---
+++ routes/api/users.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+const User = require("../models/User");
+
+router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: " Email already exist" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
+module.exports = router;
