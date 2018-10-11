ad348607e3d8f33f3c71482c5a6a0455dde9b51c
Fuzoka <unbeatablesites09@gmail.com>
2018-10-11 16:10:11 -0400

    Bug fixes

--- routes/api/users.js
+++ routes/api/users.js
@@ -38,4 +38,22 @@ router.post("/register", (req, res) => {
     }
   });
 });
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ passwrod: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
