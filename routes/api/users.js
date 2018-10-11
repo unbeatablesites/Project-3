972fbd37e5419570122247bc72b8a21b1b51b593
Fuzoka <unbeatablesites09@gmail.com>
2018-10-11 15:40:38 -0400

    registration api working

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 
@@ -11,11 +12,28 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: " Email already exist" });
     } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
