956d7e0b2dd28bcbdc4049a345a3bd30baae747e
Fuzoka <unbeatablesites09@gmail.com>
2018-10-14 22:14:16 -0400

    Working on Profiles

--- models/Profile.js
+++ models/Profile.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const ProfileSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'users'
+    ref: "users"
   },
   handle: {
     type: String,
@@ -111,3 +111,10 @@ const ProfileSchema = new Schema({
       type: String
     }
   },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
